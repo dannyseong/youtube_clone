92f3eee80b3575f8082905712b4ec0b06ddae5fe
Danny Seong <daehoon.seong@gmail.com>
2023-07-23 14:45:21 +1000

    fix: .gitignore, http>https fix

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -17,7 +17,7 @@ export default function VideoDetail() {
             width='100%'
             height='640'
             title={title}
-            src={`http://www.youtube.com/embed/${video.id}`}
+            src={`https://www.youtube.com/embed/${video.id}`}
           />
           <div className='p-8'>
             <h1 className='text-xl font-bold'>{title}</h1>
